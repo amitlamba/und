090c2119840fb7ee51a54749850e095a53429860
Kamalpreet Singh <kamalpreet.singh@userndot.com>
2018-06-06 17:43:29 +0530

    Merge branch 'master' of 13.126.53.87:/home/git/repos/und into EventUserAPI
    # Conflicts:
    #	auth-service/src/main/kotlin/com/und/config/CaptchaConfig.kt
    #	auth-service/src/main/kotlin/com/und/service/security/captcha/CaptchaService.kt
    #	auth-service/src/main/kotlin/com/und/service/security/captcha/CaptchaSettings.kt
    #	auth-service/src/main/kotlin/com/und/service/security/captcha/GoogleResponse.kt
    #	auth-service/src/main/kotlin/com/und/service/security/captcha/ICaptchaService.kt
    #	auth-service/src/main/kotlin/com/und/service/security/captcha/ReCaptchaAttemptService.kt
    #	auth-service/src/main/kotlin/com/und/web/controller/ErrorHandler/FieldError.kt
    #	auth-service/src/main/kotlin/com/und/web/controller/ErrorHandler/RestErrorHandler.kt
    #	auth-service/src/main/kotlin/com/und/web/controller/ErrorHandler/ValidationError.kt
    #	auth-service/src/main/kotlin/com/und/web/controller/errorhandler/FieldError.kt
    #	auth-service/src/main/kotlin/com/und/web/controller/errorhandler/ValidationError.kt
    #	auth-service/src/main/kotlin/com/und/web/controller/exception/ReCaptchaInvalidException.kt
    #	auth-service/src/main/kotlin/com/und/web/controller/exception/ReCaptchaUnavailableException.kt
    #	auth-service/src/main/kotlin/com/und/web/controller/exception/UserAlreadyRegistered.kt
    #	auth-service/src/main/kotlin/com/und/web/model/FieldError.kt
    #	auth-service/src/main/kotlin/com/und/web/model/ValidationError.kt
    #	auth-service/src/main/resources/messages.properties
    #	auth-service/src/main/resources/messages_en.properties
    #	auth-service/src/main/resources/messages_en_US.properties
    #	auth-service/src/main/resources/messages_es_ES.properties
    #	client-panel/src/main/kotlin/com/und/web/controller/ErrorHandler/ErrorList.kt
    #	client-panel/src/main/kotlin/com/und/web/controller/ErrorHandler/RestErrorHandler.kt
    #	client-panel/src/main/kotlin/com/und/web/controller/exception/UserNotFoundException.kt
    #	client-panel/src/main/resources/messages.properties
    #	client-panel/src/main/resources/messages_en.properties
    #	client-panel/src/main/resources/messages_en_US.properties
    #	client-panel/src/main/resources/messages_es_ES.properties

--- auth-service/src/main/kotlin/com/und/web/controller/ContactUsController.kt
+++ auth-service/src/main/kotlin/com/und/web/controller/ContactUsController.kt
@@ -2,8 +2,9 @@ package com.und.web.controller
 
 import com.und.service.ContactUsService
 import com.und.web.model.ContactUs
+import com.und.web.model.Response
+import com.und.web.model.ResponseStatus
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
 import javax.validation.Valid
@@ -18,9 +19,12 @@ class ContactUsController {
     lateinit var contactUsService: ContactUsService
 
     @PostMapping("/save")
-    fun saveContactUsDetails(@Valid @RequestBody contactInfo: ContactUs): ResponseEntity<Unit> {
+    fun saveContactUsDetails(@Valid @RequestBody contactInfo: ContactUs): ResponseEntity<Response> {
         contactUsService.save(contactInfo)
-        return ResponseEntity(HttpStatus.OK)
+        return ResponseEntity.ok().body(Response(
+                status = ResponseStatus.SUCCESS,
+                message = "Submitted Successfully"
+        ))
 
     }
 
