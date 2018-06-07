976803765393d7760c7826109ce380a611990c02
shiv <shivprataps@gmail.com>
2018-06-07 13:11:19 +0530

    removed identity from event

--- client-panel/src/main/kotlin/com/und/service/ClientUsersService.kt
+++ client-panel/src/main/kotlin/com/und/service/ClientUsersService.kt
@@ -1,8 +1,7 @@
 package com.und.service
 
-import com.und.repository.mongo.EventRepository
+import com.und.model.mongo.eventapi.EventUser
 import com.und.repository.mongo.EventUserRepository
-import com.und.model.mongo.eventapi.*
 import com.und.security.utils.TenantProvider
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Service
@@ -12,24 +11,14 @@ class ClientUsersService {
 
     @Autowired
     private lateinit var eventUserRepository: EventUserRepository
-    @Autowired
-    private lateinit var eventRepository: EventRepository
 
     fun getEventUsers(clientId: Long): List<EventUser> {
         TenantProvider().setTenat(clientId.toString())
-        var eventUsers = eventUserRepository.findAll()
-//        eventUserRepository.save(getDummyEventUser())
-//        eventUsers = listOf(getDummyEventUser())
-        return eventUsers
+        return eventUserRepository.findAll()
+
     }
 
-    fun getEventUsersEvents(clientId: Long, userId: String, clientUserId: String): List<Event> {
-        TenantProvider().setTenat(clientId.toString())
-        val identity = Identity()
-        identity.undId = userId
-        val events = eventRepository.findByIdentity(identity)
-        return events
-    }
+
 
 
 }
