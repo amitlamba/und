5610307cc6de6f9deb0b416fd5eb941921452797
Kamalpreet Singh <kamalpreet.singh@userndot.com>
2018-06-08 02:02:22 +0530

    Merge branch 'master' of 13.126.53.87:/home/git/repos/und into EventUserAPI
    # Conflicts:
    #	auth-service/src/main/kotlin/com/und/web/model/RegistrationRequest.kt
    #	auth-service/src/main/resources/messages_en_US.properties
    #	client-panel/src/main/kotlin/com/und/web/controller/EventUserController.kt
    #	client-panel/src/main/kotlin/com/und/web/controller/exception/EmailTemplateDuplicateName.kt
    #	client-panel/src/main/resources/messages_en_US.properties

--- client-panel/src/main/kotlin/com/und/web/model/Campaign.kt
+++ client-panel/src/main/kotlin/com/und/web/model/Campaign.kt
@@ -4,11 +4,19 @@ import com.und.model.CampaignStatus
 import com.und.model.jpa.CampaignType
 import com.und.model.jpa.Schedule
 import java.time.LocalDateTime
+import javax.validation.constraints.NotNull
+import javax.validation.constraints.Pattern
+import javax.validation.constraints.Size
 
 
 class Campaign {
     var id: Long? = null
+
+    @NotNull
+    @Size(min = 2, max = 50)
+    @Pattern(regexp = "[A-Za-z0-9-_][A-Za-z0-9-_\\s]*")
     var name: String = ""
+
     var schedule: Schedule? = null
     var campaignType: CampaignType? = null
     var segmentationID: Long? = null
--- client-panel/src/main/kotlin/com/und/web/model/EventUser.kt
+++ client-panel/src/main/kotlin/com/und/web/model/EventUser.kt
@@ -2,39 +2,57 @@ package com.und.web.model
 
 import java.time.LocalDateTime
 import java.util.*
+import javax.validation.constraints.Email
+import javax.validation.constraints.Pattern
+import javax.validation.constraints.Size
 
 class EventUser {
 
     var identity: Identity = Identity()
 
+    @Email
     var email: String? = null
 
     var clientUserId: String? = null //this is id of the user client has provided
 
     var undId: String? = null
 
+    @Email
     var fbId: String? = null
 
+    @Email
     var googleId: String? = null
 
-    //TODO Use custom validators here
-
+    @Size(min = 10, max = 20)
+    @Pattern(regexp = "[0-9]*")
     var mobile: String? = null
 
+    @Size(min = 2, max = 50)
+    @Pattern(regexp = "[A-Za-z0-9][A-Za-z0-9\\s]*")
     var firstName: String? = null
 
+    @Size(min = 2, max = 50)
+    @Pattern(regexp = "[A-Za-z0-9][A-Za-z0-9\\s]*")
     var lastName: String? = null
 
+    @Pattern(regexp = "[A-za-z]")
     var gender: String? = null
 
+    @Pattern(regexp="(\\d{4})[-](0?[1-9]|1[012])[-](0?[1-9]|[12][0-9]|3[01])")
     var dob: String? = null
 
+    @Size(min = 4, max = 50)
+    @Pattern(regexp = "[A-Za-z][A-Za-z\\s]*")
     var country: String? = null
 
+    @Size(min = 3, max = 50)
+    @Pattern(regexp = "[A-Za-z][A-Za-z\\s]*")
     var city: String? = null
 
+    @Size(min = 3, max = 255)
     var address: String? = null
 
+    @Pattern(regexp = "\\+[0-9]{1,3}")
     var countryCode: String? = null
 
     var clientId: Int = -1 //client id , user is associated with, this can come from collection
