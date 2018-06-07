9937f73a390f8f9f6c2cbb0c822e87817f4a8ccf
Kamalpreet Singh <kamalpreet.singh@userndot.com>
2018-06-07 13:22:04 +0530

    refactored directory errorhandler

--- auth-service/src/main/kotlin/com/und/web/controller/ErrorHandler/Error.kt
+++
@@ -1,8 +0,0 @@
-package com.und.web.controller.errorhandler
-
-//FIXME create better heirarchy of classes to report error messages
-//FIXME handle for list of error messages
-
-open class Error(
-        open var message: String
-)
