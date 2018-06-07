8dd347309b5e71545a125731649a1da14e73f231
Kamalpreet Singh <kamalpreet.singh@userndot.com>
2018-06-08 02:04:11 +0530

    Merge branch 'master' of 13.126.53.87:/home/git/repos/und into EventUserAPI
    # Conflicts:
    #	auth-service/src/main/kotlin/com/und/web/model/RegistrationRequest.kt
    #	auth-service/src/main/resources/messages_en_US.properties
    #	client-panel/src/main/kotlin/com/und/web/controller/EventUserController.kt
    #	client-panel/src/main/kotlin/com/und/web/controller/exception/EmailTemplateDuplicateName.kt
    #	client-panel/src/main/resources/messages_en_US.properties

--- client-panel/src/main/kotlin/com/und/web/controller/errorhandler/RestErrorHandler.kt
+++ client-panel/src/main/kotlin/com/und/web/controller/errorhandler/RestErrorHandler.kt
@@ -3,6 +3,7 @@ package com.und.web.controller.errorhandler
 import com.fasterxml.jackson.databind.exc.InvalidFormatException
 import com.und.common.utils.loggerFor
 import com.und.web.controller.exception.EmailTemplateDuplicateNameException
+import com.und.web.controller.exception.EventUserListNotFoundException
 import com.und.web.controller.exception.EventUserNotFoundException
 import com.und.web.controller.exception.UndBusinessValidationException
 
@@ -120,6 +121,16 @@ class RestErrorHandler : ResponseEntityExceptionHandler() {
         val bodyOfResponse = GenericResponse(messageSource.getMessage("message.eventUserNotFound", null, request.locale), ex.localizedMessage)
         return ResponseEntity(bodyOfResponse, HttpHeaders(), HttpStatus.BAD_REQUEST)
     }
+
+    @ExceptionHandler(EventUserListNotFoundException::class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ResponseBody
+    fun eventUser(ex: EventUserListNotFoundException, request: WebRequest): ResponseEntity<Any> {
+        logger.error("401 Status Code", ex)
+        val bodyOfResponse = GenericResponse(messageSource.getMessage("message.eventUserListNotFound", null, request.locale), ex.localizedMessage)
+        return ResponseEntity(bodyOfResponse, HttpHeaders(), HttpStatus.BAD_REQUEST)
+    }
+
     @ExceptionHandler(EmailTemplateDuplicateNameException::class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ResponseBody
