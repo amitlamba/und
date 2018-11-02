0e36db0986069c1b92a7fced227b6d1f29110075
jogendra shekhawat <jogendra.shekhawat@userndot.com>
2018-11-02 09:55:45 +0530

    remove conflict

--- emailsendservice/src/test/kotlin/com/und/service/FcmSendServiceTest.kt
+++ emailsendservice/src/test/kotlin/com/und/service/FcmSendServiceTest.kt
@@ -3,14 +3,15 @@ package com.und.service
 import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import com.fasterxml.jackson.module.kotlin.readValue
 import com.und.model.utils.fcm.FcmResponse
+import org.junit.Ignore
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.test.context.junit4.SpringRunner
 
-@RunWith(SpringRunner::class)
-@SpringBootTest
+//@RunWith(SpringRunner::class)
+//@SpringBootTest
 class FcmSendServiceTest {
 
     @Autowired
@@ -33,4 +34,37 @@ class FcmSendServiceTest {
 //        val jsonString = jacksonObjectMapper().writeValueAsString(response!!.body)
 //        var fcmResponse: FcmResponse = jacksonObjectMapper().readValue<FcmResponse>(jsonString)
 //    }
+    @Test
+    @Ignore
+    fun testFcmSendService() {
+//        //server key AAAAHxikc0w:APA91bHBzujPVoTOBrrkUolCzFWIsXhz-LJHYsDuw4kHCPULto7TdpZCUYfUjPTN7Ff4fYHJuacqbAQMFQa7isxwmACj89dCzKa2awR2xMZIx7QkbtU0jTOVlHe-qYKDrbiB-a2kRA0l
+//        //legacy server key AIzaSyDxc94h8UKCYhrVCcvybgd6H7PWjGaHzM4
+//        val response = fcmSendService.sendMessage(1, "AAAAHxikc0w:APA91bHBzujPVoTOBrrkUolCzFWIsXhz-LJHYsDuw4kHCPULto7TdpZCUYfUjPTN7Ff4fYHJuacqbAQMFQa7isxwmACj89dCzKa2awR2xMZIx7QkbtU0jTOVlHe-qYKDrbiB-a2kRA0l", "Hello",
+//                "Hello World", "abc")
+//        val jsonString = jacksonObjectMapper().writeValueAsString(response!!.body)
+//        var fcmResponse: FcmResponse = jacksonObjectMapper().readValue<FcmResponse>(jsonString)
+    }
+
+    @Test
+    fun sendMessage(){
+        var fcmSendService=FcmSendService()
+        var fcmMessage=FcmSendService.FcmMessage()
+        fcmMessage.project_id="cloudmessaging-d2f78"
+        var message=TestMessage()
+        var m=FcmSendService.TestMessage1()
+//        message.token="APA91bFnYNQZj-d5iWVkzDc0xTFf27ag97mC14VxIFkO2kVrL2tYFsjvit_I6pCvMF_tVJGrS01lszCCg7BJPLmO_HuA1deRJtdloKN2nuMUXOz472-uCjw"
+        message.token="eEpmrv70drA:APA91bH8uGOUHhTi5AFMJuMx7tQ49vnQkphMNryaXE9IMV47MVQ8FDyaz2sSztyeGTxSZtpI1kA7hii-MnvFzq1Nh6IpdT8bhfaprjscWLNJfAxO3Wz3wQmhtZDB6BiIX2llcaVdjiAx"
+        var map=HashMap<String,String>()
+        map.put("channel_id","userndot")
+        map.put("title","Httpv1 test")
+        map.put("body","Http v1 test successfull")
+        map.put("channel_name","userndotc")
+        map.put("pr","high")
+        map.put("bg_pic","")
+        map.put("userndot","yes")
+        message.data=map
+        m.message=message
+        fcmMessage.message=m
+        fcmSendService.sendMessage(fcmMessage)
+    }
 }
