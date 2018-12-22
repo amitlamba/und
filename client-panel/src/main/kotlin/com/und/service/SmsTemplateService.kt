75226aaca54a573f074e2a0b8f499565715615e9
jogendra shekhawat <jogendra.shekhawat@userndot.com>
2018-12-22 15:28:48 +0530

    add in sms

--- client-panel/src/main/kotlin/com/und/service/SmsTemplateService.kt
+++ client-panel/src/main/kotlin/com/und/service/SmsTemplateService.kt
@@ -3,10 +3,13 @@ package com.und.service
 import com.und.model.jpa.SmsTemplate
 import com.und.model.Status
 import com.und.repository.jpa.SmsTemplateRepository
+import com.und.security.utils.AuthenticationUtils
 import com.und.web.controller.exception.CustomException
 import org.hibernate.exception.ConstraintViolationException
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.dao.DataIntegrityViolationException
+import org.springframework.security.access.AccessDeniedException
+import org.springframework.security.core.AuthenticationException
 import org.springframework.stereotype.Service
 
 @Service
@@ -40,5 +43,12 @@ class SmsTemplateService {
 
     fun getUserEventAttributes() {
         TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    fun getSmsTemplateById(id:Long):SmsTemplate{
+        var clientID=AuthenticationUtils.clientID?:throw AccessDeniedException("")
+        var template=smsTemplateRepository.findByIdAndClientID(id,clientID)
+        if(template.isPresent) return template.get()
+        return throw CustomException("No template with id ${id} exists")
     }
 }
