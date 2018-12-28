d23675c438f9f45dc8bb598b7a0a032f75894024
jogendra shekhawat <jogendra.shekhawat@userndot.com>
2018-12-28 15:26:43 +0530

    jackson handling

--- client-panel/src/main/kotlin/com/und/web/model/ServiceProviderCredentials.kt
+++ client-panel/src/main/kotlin/com/und/web/model/ServiceProviderCredentials.kt
@@ -1,10 +1,14 @@
 package com.und.web.model
 
+import com.fasterxml.jackson.annotation.JsonGetter
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties
+import com.fasterxml.jackson.annotation.JsonProperty
 import com.fasterxml.jackson.annotation.JsonSetter
 import com.und.model.Status
 import java.util.*
 import javax.validation.constraints.NotNull
 
+@JsonIgnoreProperties("default")
 class ServiceProviderCredentials {
     var id: Long? = null
     var clientID: Long? = null
@@ -21,7 +25,11 @@ class ServiceProviderCredentials {
 
     var credentialsMap: HashMap<String, String> = HashMap()
 
-    @JsonSetter("isDefault")
+    @JsonProperty("isDefault")
     var isDefault:Boolean=false
+//        @JsonGetter("isDefault")
+//    get() =isDefault?:false
+//        @JsonSetter("isDefault")
+//    set(value) {isDefault=value}
 
 }
