dbd4c54565273eda50e9c63122a66dda5d05e358
shiv <shivprataps@gmail.com>
2019-01-11 12:19:00 +0530

    insert metadata only if it exists

--- auth-service/src/main/kotlin/com/und/repository/jpa/ClientRepositoryCustom.kt
+++ auth-service/src/main/kotlin/com/und/repository/jpa/ClientRepositoryCustom.kt
@@ -1,6 +1,8 @@
 package com.und.repository.jpa
 
 interface ClientRepositoryCustom {
+    fun userpropertiesExists(clientId:Long):Boolean
+    fun eventMetadtaExists(clientId:Long):Boolean
     fun saveUserProperties(clientId: Long, metadataJson: String)
     fun saveEventMetadta(clientId: Long, metadataJson: String)
 }
--- auth-service/src/main/kotlin/com/und/repository/jpa/ClientRepositoryCustomImpl.kt
+++ auth-service/src/main/kotlin/com/und/repository/jpa/ClientRepositoryCustomImpl.kt
@@ -4,27 +4,40 @@ import com.mongodb.BasicDBList
 import com.mongodb.BasicDBObject
 import com.mongodb.DBObject
 import com.und.model.mongo.CommonMetadata
-import com.und.model.mongo.EventMetadata
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.data.mongodb.core.MongoOperations
 import org.springframework.data.mongodb.core.MongoTemplate
+import org.springframework.data.mongodb.core.query.Criteria
+import org.springframework.data.mongodb.core.query.Query
 
 class ClientRepositoryCustomImpl : ClientRepositoryCustom {
 
     @Autowired
     lateinit var mongoTemplate: MongoTemplate
 
+    @Autowired
+    lateinit var mongoOperations: MongoOperations
+
     override fun saveUserProperties(clientId: Long, metadataJson: String) {
 
-        val dbObject : DBObject = BasicDBObject.parse(metadataJson) as DBObject
+        val dbObject: DBObject = BasicDBObject.parse(metadataJson) as DBObject
         val parsed = dbObject.get("userProperties") as BasicDBList
         mongoTemplate.insert(parsed, "${clientId}_userproperties")
 
     }
 
     override fun saveEventMetadta(clientId: Long, metadataJson: String) {
-        val dbObject : DBObject = BasicDBObject.parse(metadataJson) as DBObject
+        val dbObject: DBObject = BasicDBObject.parse(metadataJson) as DBObject
         val parsed = dbObject.get("eventMetadata") as BasicDBList
-        mongoTemplate.insert(parsed,  "${clientId}_eventmetadata")
+        mongoTemplate.insert(parsed, "${clientId}_eventmetadata")
 
+    }
+
+    override fun userpropertiesExists(clientId: Long): Boolean {
+        return mongoOperations.exists(Query(Criteria.where("name").`is`("Technographics").exists(true)), "${clientId}_userproperties")
+    }
+
+    override fun eventMetadtaExists(clientId: Long): Boolean {
+        return mongoOperations.exists(Query(Criteria.where("name").exists(true)), "${clientId}_eventmetadata")
     }
 }
