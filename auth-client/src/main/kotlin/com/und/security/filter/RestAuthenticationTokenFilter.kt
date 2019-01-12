f038d2d1c61a50f322fef3330c16492eabaedee8
jogendra shekhawat <jogendra.shekhawat@userndot.com>
2019-01-12 16:00:47 +0530

    jogendra

--- auth-client/src/main/kotlin/com/und/security/filter/RestAuthenticationTokenFilter.kt
+++ auth-client/src/main/kotlin/com/und/security/filter/RestAuthenticationTokenFilter.kt
@@ -12,6 +12,7 @@ import org.springframework.security.web.authentication.WebAuthenticationDetailsS
 import org.springframework.web.filter.OncePerRequestFilter
 import java.io.IOException
 import javax.servlet.FilterChain
+import java.util.regex.Pattern
 import javax.servlet.ServletException
 import javax.servlet.http.HttpServletRequest
 import javax.servlet.http.HttpServletResponse
