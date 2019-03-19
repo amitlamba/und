961e12655a4e1d421b49a4b3ed8d406e254351e9
jogendra shekhawat <jogendra.shekhawat@userndot.com>
2019-03-19 17:54:50 +0530

    testcampaign web model

---
+++ client-panel/src/main/kotlin/com/und/web/model/TestCampaign.kt
@@ -0,0 +1,23 @@
+package com.und.web.model
+
+import com.und.model.CampaignStatus
+import com.und.model.jpa.CampaignType
+import com.und.model.jpa.SmsTemplate
+import java.time.LocalDateTime
+import javax.validation.constraints.NotNull
+
+class TestCampaign {
+    var emailTemplate:EmailTemplate?=null
+    var smsTemplate:SmsTemplate?=null
+    var androidTemplate:AndroidTemplate?=null
+    var webPushTemplate:WebPushTemplate?=null
+//    var iosPushTemplate:IosPushTemplate
+    var findByType:String?=null
+    var toAddresses:String?=null
+    @NotNull
+    lateinit var campaignType: CampaignType
+    var segmentationID: Long?=null
+    var serviceProviderId:Long?=null
+    var fromUser:String?=null
+    var clientEmailSettingId:Long?=null
+}
