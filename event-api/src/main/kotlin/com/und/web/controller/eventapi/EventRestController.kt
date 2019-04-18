a2b4e03a42988b06b8ff1d5810a9641b2abc098f
Jogender <jogender@macair.local>
2019-04-18 15:44:23 +0530

    fix mapping

--- event-api/src/main/kotlin/com/und/web/controller/eventapi/EventRestController.kt
+++ event-api/src/main/kotlin/com/und/web/controller/eventapi/EventRestController.kt
@@ -89,7 +89,9 @@ class EventRestController {
 
 
         identityInit.clientId = tenantProvider.tenant.toInt()
-//        eventUser.identity = identityInit
+        if(eventUser.identity.userId==null){
+        eventUser.identity = identityInit
+        }
 
 
         if (uid != null && uid.isNotEmpty()) {
