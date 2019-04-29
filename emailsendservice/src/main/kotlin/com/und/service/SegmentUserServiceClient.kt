df326886ad7a3042c69fe0dc36e55e0c24757c10
jogendra Singh Shekhawat <jogendra.shekhawat@userndot.com>
2019-04-29 10:26:35 +0530

    change feginn client url

--- emailsendservice/src/main/kotlin/com/und/service/SegmentUserServiceClient.kt
+++ emailsendservice/src/main/kotlin/com/und/service/SegmentUserServiceClient.kt
@@ -6,8 +6,8 @@ import org.springframework.cloud.openfeign.FeignClient
 import org.springframework.web.bind.annotation.*
 
 
-//@FeignClient(name = "client-panel", value = "client-panel")
-@FeignClient(name = "client-panel",url = "http://localhost:9201")
+@FeignClient(name = "client-panel", value = "client-panel")
+//@FeignClient(name = "client-panel",url = "http://localhost:9201")
 interface SegmentUserServiceClient {
 
     //FIXME define a new role and key here from config
